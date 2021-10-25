1ff9c29cda9e307d7d05bc6ca7c9ae001b59cdff
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-25 16:05:23 +0200

    card complete in html

---
+++ main.js
@@ -0,0 +1,15 @@
+const accounts = [
+  'Olivia Rodrigo',
+  'Nasus il Gattopardo',
+  'Yoda il Temibile',
+  'Bubba dall\'Uganda'
+]
+
+const card = [
+  {
+    accountImage: 'https://picsum.photos/200',
+    accountName: 'Olivia Rodrigo',
+    imagePost: 'https://picsum.photos/500',
+    likes: ''
+  }
+]
